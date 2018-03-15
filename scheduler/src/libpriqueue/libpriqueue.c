8037e358e0123d47680ab4ebff2a820e9b7b1739
Johnathon Bliss <jbliss9821@gmail.com>
2018-03-15 13:55:38 -0500

    Add definition for prequeue_poll

--- scheduler/src/libpriqueue/libpriqueue.c
+++ scheduler/src/libpriqueue/libpriqueue.c
@@ -70,7 +70,26 @@ void *priqueue_peek(priqueue_t *q)
  */
 void *priqueue_poll(priqueue_t *q)
 {
-	return NULL;
+	void* data = q -> head -> value;
+	if( q -> size == 0)
+		{
+			return NULL;
+		}
+	else if(q -> size == 1)
+	{
+		free (q -> head);
+		q -> head = NULL:
+		q -> size--;
+		return data;
+	}
+	else
+	{
+		struct node_t* temp = q -> head;
+		q -> head = q -> head -> next;
+		free (q -> head);
+		q -> size--;
+		return data;
+	}
 }
 
 
@@ -85,7 +104,23 @@ void *priqueue_poll(priqueue_t *q)
  */
 void *priqueue_at(priqueue_t *q, int index)
 {
-	return NULL;
+	struct node_t* temp = q -> head;
+	if(q -> size == 0)
+	{
+		return NULL;
+	}
+	else if (index >= q-> size || index < 0)
+	{
+		return NULL;
+	}
+	 else
+		{
+		for (int i = 0; i < index; i++)
+		{
+			temp = temp -> next;
+		}
+		return temp -> data;;
+		}
 }
 
 
